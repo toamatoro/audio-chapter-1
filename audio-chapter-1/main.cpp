faf2322e3f166d4493bf8fa4d09c01710b4de888
toamatoro <90988256+toamatoro@users.noreply.github.com>
2022-03-23 20:40:12 -0400

    Initial Commit

---
+++ audio-chapter-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  audio-chapter-1
+//
+//  Created by Jonathan Lausch on 3/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
